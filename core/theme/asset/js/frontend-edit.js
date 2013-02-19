aa80081e5e29fa585efac80565614258a954c7f2
Brad Touesnard <brad@touesnard.com>
2013-02-19 10:56:48 -0400

    Fixes #1: Content Editor Highlighting

--- core/theme/asset/js/frontend-edit.js
+++ core/theme/asset/js/frontend-edit.js
@@ -19,46 +19,56 @@
 			$(this).removeAttr('data-lf-edit');
 		});
 
+		var boxes = [];
 		$('[data-lf-edit]').each(function() {
-			var $container = $(this),
-				fieldsets = $container.data('lf-edit'),
-				offset = $container.offset(),
-				width = $container.outerWidth(),
-				height = $container.outerHeight();
+			var box = {};
+			box.$container = $(this);
+			box.$pad = $('<div class="lf-edit-box"></div>');
+			box.$pad.appendTo('.lf-edit-container');
 
-			if (width <= 20) {
-				width = 20;
-			}
-
-			if (height <= 20) {
-				height = 20;
-			}
-
-			var top = offset.top - 20,
-				left = offset.left - 20;
-			
-			width = width + 40;
-			height = height + 40;
-
-			var $box = $('<div class="lf-edit-box"></div>');
-			$box.css({
-				'top': top + 'px',
-				'left': left + 'px',
-				'width': width + 'px',
-				'height': height + 'px' 
-			});
-			$box.appendTo('.lf-edit-container');
-
-			$box.click(function() {
-				window.parent.leeflets.load_content_panel(fieldsets);
+			box.$pad.click(function() {
+				window.parent.leeflets.load_content_panel(box.$container.data('lf-edit'));
 				return false;
 			});
 
-			var edit_title = $container.data('lf-edit-title');
-			if (edit_title) {
-				$box.append('<h1>' + edit_title + '</h1>');
-			}
+			boxes.push(box);
 		});
+
+		function size_position_boxes() {
+			$.each(boxes, function() {
+				var box = this,
+					offset = box.$container.offset(),
+					width = box.$container.outerWidth(),
+					height = box.$container.outerHeight();
+
+				if (width <= 20) {
+					width = 20;
+				}
+
+				if (height <= 20) {
+					height = 20;
+				}
+
+				var top = offset.top - 20,
+					left = offset.left - 20;
+				
+				width = width + 40;
+				height = height + 40;
+
+				box.$pad.css({
+					'top': top + 'px',
+					'left': left + 'px',
+					'width': width + 'px',
+					'height': height + 'px'
+				});
+			});
+		}
+
+		size_position_boxes();
+		on_resize(size_position_boxes);
 	});
 
 })(jQuery);
+
+// debulked onresize handler
+function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,100)};return c};
