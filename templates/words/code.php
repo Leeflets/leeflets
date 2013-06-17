0f3e5523061f846a4003e2e50657630c33658e2c
Brad Touesnard <brad@touesnard.com>
2013-06-17 11:30:58 -0300

    Remove old template.

--- templates/words/code.php
+++
@@ -1,5 +0,0 @@
-<?php
-$this->enqueue_script( 'words-script', 'js/script.js', array( 'jquery' ) );
-$this->enqueue_style( 'words-base', 'css/base.css' );
-$this->enqueue_style( 'words-style', 'css/style.css' );
-$this->enqueue_style( 'googlefonts-merriweather', 'http://fonts.googleapis.com/css?family=Merriweather' );
