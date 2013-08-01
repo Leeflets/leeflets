ee2eee55932e7f6c12201e6dd40b10efcd77d706
Brad Touesnard <brad@touesnard.com>
2013-08-01 15:40:59 -0300

    Bug: Google font not loading in Koala template

--- templates/koala/code.php
+++ templates/koala/code.php
@@ -5,6 +5,6 @@ class Code extends \Leeflets\Template\Code {
 	function setup_hooks() {
 		$this->enqueue_style( 'koala-style', 'style.css' );
 		$this->enqueue_style( 'koala-grid', 'subdiv.css' );
-		$this->enqueue_style( 'googlefonts-Varela+Round', 'http://fonts.googleapis.com/css?family=Varela+Round' );
+		$this->enqueue_style( 'googlefonts-Varela+Round', 'http://fonts.googleapis.com/css?family=Varela+Round', array(), null );
 	}
 }
